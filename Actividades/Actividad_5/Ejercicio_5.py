5cfe3a9a864c30816f8ff9c88e6962bdadae1744
John <jbarragandela@nigul.cide.es>
2023-10-24 18:11:36 +0200

    arreglado sin el texto ruby

--- Actividades/Actividad_5/Ejercicio_5.py
+++ Actividades/Actividad_5/Ejercicio_5.py
@@ -7,18 +7,3 @@ else:
     for i in range (1, numero +1):
         factorial *= i
 print("El factorial de ", numero, " es ", factorial)
-
-# uf que codigo mas raro, no has pensado en usa ruby?¿ mira un ej : 
-# class Mamifero 
-#  def respira
-#    puts "inhala y exhala"
-#  end
-# end
-# class Gato<Mamifero
-#  def habla
-#    puts "Meow"
-#  end
-# end
-# misifus = Gato.new
-# misifus.respira
-# misifus.habla
