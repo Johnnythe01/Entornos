8d14e66cde6e3c3d50f17a08b960168084e49d91
John <jbarragandela@nigul.cide.es>
2023-12-18 20:44:55 +0100

    desastre absoluto

--- Actividades/Space_Invaders/elementos2.py
+++ Actividades/Space_Invaders/elementos2.py
@@ -1,5 +1,6 @@
+from typing import Any
 import pygame
-from pygame.sprite import Group
+from pygame.sprite import Group, Group
 
 class Nave(pygame.sprite.Sprite):
     # constructor
@@ -7,7 +8,7 @@ class Nave(pygame.sprite.Sprite):
         super().__init__()
         # cargamos la imagen
         self.manolos = pygame.image.load("avion.png"), pygame.image.load("avion2.png")
-        imagen = (pygame.image.load("avion.png"), pygame.image.load("avion2.png"))
+        #imagen = (pygame.image.load("avion.png"), pygame.image.load("avion2.png"))
         self.indice_manolos = 0
         self.image = self.manolos [self.indice_manolos]
         self.contador_imagen = 0
@@ -32,7 +33,7 @@ class Nave(pygame.sprite.Sprite):
             pantalla = pygame.display.get_surface()
         # gestionamos la animacion
         self.contador_imagen = (self.contador_imagen +1) % 40
-        self.indice_manolo = self.contador_imagen //20
+        self.indice_manolo = self.contador_imagen // 20
         self.image = self.manolos[self.indice_manolo]
             
         if teclas[pygame.K_UP]:
@@ -40,6 +41,40 @@ class Nave(pygame.sprite.Sprite):
         if teclas[pygame.K_DOWN]:
             self.rect.y += 2
 
+            pantalla = pygame.display.get_surface()
+        if (self.rect.y > pantalla.get_height() -200):
+            self.kill()
+
+class Fondo(pygame.sprite.Sprite):
+    def __init__():
+        super().__init__()
+        # cargamos la imagen
+        manolo = pygame.image.load("space.jpg")
+        # capturamos la pantalla
+        pantalla = pygame.display.get_surface()
+        imagen_cargada, (pantalla.get_width(, imagen_cargada.get_height()))
+        
+        # creamos un rectangulo a partir de la imagen
+        self.rect = self.image.get_rect()
+        # actualizar la posicion del rectangulo para que coincida con "posicion"
+        self.rect.topleft = (0,0)
+
+class Bala(pygame.sprite.Sprite):
+    def __init__(self, posicion) -> None:
+        super().__init__
+        # creamos un rectangulo
+        self.image = pygame.Surface((5,10))
+        self.image.fil ((255,0,0))
+        self.rect = self.image.get_rect()
+        self.rect.center = posicion
+
+    def disparar(self, grupo_sprites):
+        momento_actual = pygame.time.get_ticks
+        bala = Bala ((self.rect.x + self.image.get_width() /2, self.rect.y))
+        grupo_sprites.add(bala)
+
+    def update(self, *args: Any, **kwargs: Any) -> None:
+        self.rect.y -= 5
 
 class Enemigo(pygame.sprite.Sprite):
     # constructor
--- Actividades/Space_Invaders/spaceInvaders2.py
+++ Actividades/Space_Invaders/spaceInvaders2.py
@@ -15,15 +15,15 @@ FPS = 60
 
 # booleano de control
 running = True
-posicion = (200,200)
-nave = elementos2.Nave(posicion)
+
+nave = elementos2.Nave((400,500))
 # nave = Elementos2.Nave((200,200))
 
 # creamos un grupo de sprites
 grupo_sprites = pygame.sprite.Group(nave)
 grupo_sprites.add(elementos2.Nave((100,100)))
+grupo_sprites.add(elementos2.Nave((420,100)))
 grupo_sprites.add(elementos2.Nave((300,100)))
-grupo_sprites.add(elementos2.Nave((400,100)))
 
 enemigo = elementos2.Enemigo((50,50))
 grupo_sprites.add(enemigo)
@@ -43,17 +43,20 @@ while running:
 
     # creacion de enemigos
     momento_actual = pygame.time.get_ticks()
-    if (momento_actual > ultimo_enemigo_creado + 60000):
+    if (momento_actual > ultimo_enemigo_creado + 6000):
         coordX = random.randint(0,pantalla.get_width())
-        coordY = 0
+        coordY = -200
         grupo_sprites.add(elementos2.Enemigo((coordX, coordY)))
         ultimo_enemigo_creado = momento_actual
+    
+
     #capturamos las teclas
     teclas = pygame.key.get_pressed()
 
     # pintaremos
     # primero el fondo blanco
     pantalla.fill((255,255,255))
+
     # segundo los sprites
     grupo_sprites.update(teclas)
     grupo_sprites.draw(pantalla)
